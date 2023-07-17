cb5e34081c6991691af3c9268bf34156ca8b9d5a
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-07-17 20:33:11 +0200

    Create seeds

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name: "Test User", email: "1@gmail.com", password: "1", password_confirmation: "1")
+Room.create(name: "Test Room")
+Room.first.messages.create(text: "Hi!", user_id: User.first.id)
